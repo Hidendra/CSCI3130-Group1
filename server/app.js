d745627074cb74ac75e1f732f39c4184f3e759a4
Tyler Blair <hidendra@griefcraft.com>
2014-01-27 18:10:32 -0400

    Connect to mongodb with some examples in app.js
    included all npm modules so it can be ran anywhere easily

--- server/app.js
+++ server/app.js
@@ -1,12 +1,40 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function(req, res){
+// connect to Mongo
+var db = require('monk')('127.0.0.1:27017/group1');
+var testcollection = db.get('testcollection');
+
+app.get('/', function(req, res) {
     res.send('Hello World');
 });
 
-app.get('/test/:x', function(req, res){
+app.get('/test/:x', function(req, res) {
     res.send('test: ' + req.params.x);
+});
+
+app.get('/find', function(req, res) {
+    testcollection.find({}, function(err, docs) {
+        res.json(docs);
+    });
+});
+
+app.get('/insert/:name', function(req, res) {
+    testcollection.insert({
+        name: req.params.name
+    });
+
+    console.log("Inserted: " + req.params.name);
+    res.json({status: 'ok'});
+});
+
+app.get('/remove/:name', function(req, res) {
+    testcollection.remove({
+        name: req.params.name
+    });
+
+    console.log("Removed: " + req.params.name);
+    res.json({status: 'ok'});
 });
 
 app.listen(8001);
