37cbe116c6e5803188d7d7e81b41b9fec9be19ae
Tyler Blair <hidendra@griefcraft.com>
2014-01-31 20:54:18 +0000

    formatting cleanup and add .gitignore

--- server/app.js
+++ server/app.js
@@ -5,63 +5,62 @@ app.use(express.bodyParser());
 
 // connect to Mongo
 var db = require('monk')('127.0.0.1:27017/group1');
-var testcollection = db.get('testcollection');
 var users = db.get('users');
 
 // Get a listing of all users
-app.get('/user', function(req, res) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    users.find({}, function(err, docs) {
-       res.json(docs);
-    });
+app.get('/user', function (req, res) {
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	users.find({}, function (err, docs) {
+		res.json(docs);
+	});
 });
 
 // Verify a user's credentials
-app.post('/user/:username', function(req, res) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    users.find({
-        username: req.params.username
-    }, function(err, docs) {
-        if (docs.length == 0) {
-            res.send(404);
-        } else {
-            var user = docs[0];
-            if (sha1(req.body.password) == user.password) {
-                res.json(user);
-            } else {
-                res.send(403);
-            }
-        }
-    });
+app.post('/user/:username', function (req, res) {
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	users.find({
+		username: req.params.username
+	}, function (err, docs) {
+		if (docs.length == 0) {
+			res.send(404);
+		} else {
+			var user = docs[0];
+			if (sha1(req.body.password) == user.password) {
+				res.json(user);
+			} else {
+				res.send(403);
+			}
+		}
+	});
 });
 
 // Create a user
-app.post('/user', function(req, res) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
+app.post('/user', function (req, res) {
+	res.setHeader('Access-Control-Allow-Origin', '*');
 
-    var username = req.body.username;
+	var username = req.body.username;
 
-    // check that the users did not
-    users.find({
-        username: username
-    }, function(err, docs) {
-        if (docs.length != 0) {
-            res.send(403);
-            return;
-        }
+	// check that the users did not
+	users.find({
+		username: username
+	}, function (err, docs) {
+		if (docs.length != 0) {
+			res.send(403);
+			return;
+		}
 
-        users.insert({
-            username: username,
-            password: sha1(req.body.password)
-        });
+		users.insert({
+			username: username,
+			password: sha1(req.body.password)
+		});
 
-        // output the uesr back to the client
-        users.find({
-           username: username
-        }, function(err, docs) {
-           res.json(docs);
-        });
-    });
+		// output the user back to the client
+		users.find({
+			username: username
+		}, function (err, docs) {
+			res.json(docs);
+		});
+	});
 });
 
 app.listen(8001);
--- web/login.js
+++ web/login.js
@@ -1,80 +1,75 @@
+var userLogin;
+var password;
 
-	var userLogin;
-        var password;
-
-        var existingUser = function(){
-            userLogin = prompt("Enter your user name: ", "Print here!");
-			if(userLogin == null){
-				return;
-			}
-            password = prompt("Enter your password: ", "Print here!");
-			if(password == null){
-				return;
-			}
-
-            $.post('http://centi.cs.dal.ca:8001/user/' + userLogin, {
-                password: password
-            }, function(data) {
-                console.log(data);
-            }).fail(function(e) {
-                if (e.status == 404 || e.status == 403) {
-                    alert('Invalid user/pw');
-                }
-            });
+var existingUser = function () {
+	userLogin = prompt("Enter your user name: ", "Print here!");
+	if (userLogin == null) {
+		return;
+	}
+	password = prompt("Enter your password: ", "Print here!");
+	if (password == null) {
+		return;
 	}
 
-        var newUser = function(){
-	    //Boolean variable to check if the username is taken
-	    var nameTaken = false;
-
-	    //sample name for testing
-	    var sampleName = "A";
-
-	//create the new user and check for duplication    
-	do{
-	    userLogin = prompt("Enter your desired user name: ", "Print here!");
-		if(userLogin == null){
-				return;
+	$.post('http://centi.cs.dal.ca:8001/user/' + userLogin, {
+		password: password
+	},function (data) {
+		console.log(data);
+	}).fail(function (e) {
+			if (e.status == 404 || e.status == 403) {
+				alert('Invalid user/pw');
 			}
+		});
+}
 
-       	    //if dupe, alert user that the name was taken and try again
- 	    if (userLogin === sampleName){
-		alert("NAME TAKEN");
-		nameTaken=true;
+var newUser = function () {
+	//Boolean variable to check if the username is taken
+	var nameTaken = false;
 
-	    //if the username was not taken
-	    }else{
-		nameTaken=false;
-	    }
-	    //this is a querry seach in the database and if its not there, create the new user. 
-	    
-	    }while(nameTaken===true);
+	//sample name for testing
+	var sampleName = "A";
 
-	    password = prompt("Enter your password:","Print here!");
-		if(password == null){
-		        //remove the name from the database
-				return;
-			}
+	//create the new user and check for duplication
+	do {
+		userLogin = prompt("Enter your desired user name: ", "Print here!");
+		if (userLogin == null) {
+			return;
+		}
 
-	
-            //ENTER USER INFORMATION INTO DB
-            $.post('http://centi.cs.dal.ca:8001/user', {
+		//if dupe, alert user that the name was taken and try again
+		if (userLogin === sampleName) {
+			alert("NAME TAKEN");
+			nameTaken = true;
+
+			//if the username was not taken
+		} else {
+			nameTaken = false;
+		}
+		//this is a querry seach in the database and if its not there, create the new user.
+
+	} while (nameTaken === true);
+
+	password = prompt("Enter your password:", "Print here!");
+	if (password == null) {
+		//remove the name from the database
+		return;
+	}
+
+
+	//ENTER USER INFORMATION INTO DB
+	$.post('http://centi.cs.dal.ca:8001/user', {
 		username: userLogin,
 		password: password
-            }, function(data) {
+	},function (data) {
 		console.log(data);
-            }).fail(function(e) {
-               if (e.status == 403) {
-                   alert("user already exists");
-               }
-            });
+	}).fail(function (e) {
+			if (e.status == 403) {
+				alert("user already exists");
+			}
+		});
 
-	}
-
-
-	
+}
 
 
-	
-//fns which loads things in the background, fns which deal with buttons, fns which 
+//fns which loads things in the background, fns which deal with buttons, fns which
 //take in text for user/password, fns that true or false login info,fn for new user	
