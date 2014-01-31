4c59f3bcce6f10fae4ec7fb654cb33daee849e95
Tyler Blair <hidendra@griefcraft.com>
2014-01-31 17:50:52 -0400

    Session handling so authentication over REST can be handled

---
+++ server/session.js
@@ -0,0 +1,45 @@
+var crypto = require('crypto');
+
+module.exports = function (db) {
+	var sessions = db.get('sessions');
+
+	return {
+		// Finds a session and returns the user it is for, otherwise null.
+		findSession: function (sessionKey) {
+			sessions.find({
+				key: sessionKey
+			}, function (err, docs) {
+				if (docs.length == 0) {
+					return null;
+				} else {
+					return docs.userid;
+				}
+			});
+		},
+
+		// Creates a new session and returns the session id
+		createSession: function (userid) {
+			var key = generateKey();
+
+			// expire any older sessions for the user
+			sessions.remove({
+				userid: userid
+			});
+
+			// insert the new session
+			sessions.insert({
+				userid: userid,
+				key: key
+			});
+
+			return key;
+		}
+	}
+}
+
+// Generate a session key
+var generateKey = function () {
+	var sha = crypto.createHash('sha256');
+	sha.update(Math.random().toString());
+	return sha.digest('hex');
+};
