7b88eaa83f40d8b8b5877d4617f1232c6db881e3
June1993 <june.yang1993@gmail.com>
2014-02-07 15:50:07 -0800

    put in fixed buttons

--- web/hotspot.js
+++ web/hotspot.js
@@ -2,16 +2,48 @@ var apiUrl = 'http://centi.cs.dal.ca:8001';
 var sessionkey = null;
 
 var existingUser = function () {
-	var username = null;
-	var password = null;
+	/**
+    * Set the default values for username and password to be empty string
+    * So it won't conflict to null value which is returned when cancle or close button is hit
+	*/
+	var username = "";
+	var password = "";
 
-	while (username == null) {
+	/**
+	*if no input for username, use a prompt to ask for it
+	*/
+	username = prompt('Enter your username:');
+	while (username == "") {
+		alert("Your username cannot be empty");
 		username = prompt('Enter your username:');
+		if(username == null){
+			break;
+		}
 	}
+    
+    /** 
+    * null value will be returned if cancle or close button is hit
+    * if the username is null, set the password to null 
+    * So it won't have the prompt box for password
+    */
+	if(username == null)
+		password = null;
 
-	while (password == null) {
-		password = prompt('Enter your password:');
-	}
+   if(password != null){
+   	    password = prompt('Enter your password:');
+	    while (password == "") {
+		    alert("Your password cannot be empty");
+		    password = prompt('Enter your password:');
+		    if(password == null){
+			    break;
+			}
+	    }
+   }
+   
+   if( password == null ){
+   		e.status = 404;
+   }
+    
 
 	$.post(apiUrl + '/user/' + username, {
 		password: password
@@ -19,34 +51,56 @@ var existingUser = function () {
 		completeLogin(data);
 
 	}).fail(function (e) {
-			if (e.status == 404 || e.status == 403) {
-				alert('Invalid user/pw');
+			if (e.status == 404 || e.status == 403){
+				if((username != null) && (password!= null)) {
+				    alert('Invalid user/pw');
+				}    
 			}
 		});
 }
 
 var completeLogin = function (data) {
-		sessionkey = data.key;
-		console.log(data);
-		alert('Welcome!');
-		$("#signup").hide();
-		$("#signin").hide();
-     $('#map').removeClass('hidden');
- 
+	sessionkey = data.key;
+	console.log(data);
+	alert('Welcome!');
+	$("#signup").hide();
+	$("#signin").hide();
+    $('#map').removeClass('hidden');
 }
 
 var newUser = function () {
-	var username = null;
-	var password = null;
+	var username = "";
+	var password = "";
 
 	//create the new user and check for duplication
-	while (username == null) {
+
+	username = prompt('Enter your desired username:');
+	while (username == "")  {
+		alert("Your username cannot be empty");
 		username = prompt('Enter your desired username:');
+		if(username == null){
+			break;
+		}
 	}
 
-	while (password == null) {
-		password = prompt('Enter your password:');
-	}
+	if(username == null)
+		password = null;
+
+	if(password != null){
+   	    password = prompt('Enter your password:');
+	    while (password == "") {
+		    alert("Your password cannot be empty");
+		    password = prompt('Enter your password:');
+		    if(password == null){
+		    	break;
+		    }
+	    }
+   }
+
+   if( password == null ){
+   		e.status = 403;
+   }
+	
 
 	$.post(apiUrl + '/user', {
 		username: username,
@@ -56,7 +110,9 @@ var newUser = function () {
 
 	}).fail(function (e) {
 			if (e.status == 403) {
-				alert('user already exists');
+				if((username != null) && (password!= null)){
+				    alert('user already exists');
+				}
 			}
 		});
 }
@@ -81,5 +137,5 @@ var watchLocation = function () {
 
 
 
-//fns which loads things in the background, fns which deal with buttons, fns which
+//fns which loads things in the background,  fns which
 //take in text for user/password, fns that true or false login info,fn for new user	
