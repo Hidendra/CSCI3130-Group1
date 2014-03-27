d6282eb3b87c8ac250965e7d3c415608b0a21c06
Joel Violette <joeliusmagnus@gmail.com>
2014-03-27 17:52:40 -0300

    added append places button to myplaces as per story 6

--- server/app.js
+++ server/app.js
@@ -25,143 +25,143 @@ var session = require('./session')(db);
 
 // Get a listing of all users
 app.get('/user', function (req, res) {
-	res.setHeader('Access-Control-Allow-Origin', '*');
-	users.find({}, function (err, docs) {
-		res.json(docs);
-	});
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    users.find({}, function (err, docs) {
+        res.json(docs);
+    });
 });
 
 // Login a user
 app.post('/user/:username', function (req, res) {
-	res.setHeader('Access-Control-Allow-Origin', '*');
-	users.find({
-		username: req.params.username
-	}, function (err, docs) {
-		if (docs.length == 0) {
-			res.send(404);
-		} else {
-			var user = docs[0];
-			var key = session.createSession(user._id);
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    users.find({
+        username: req.params.username
+    }, function (err, docs) {
+        if (docs.length == 0) {
+            res.send(404);
+        } else {
+            var user = docs[0];
+            var key = session.createSession(user._id);
 
-			console.log('User ' + user.username + ' authenticated. Session id: ' + key);
+            console.log('User ' + user.username + ' authenticated. Session id: ' + key);
 
-			if (sha1(req.body.password) == user.password) {
-				res.json({
-					key: key
-				});
-			} else {
-				res.send(403);
-			}
-		}
-	});
+            if (sha1(req.body.password) == user.password) {
+                res.json({
+                    key: key
+                });
+            } else {
+                res.send(403);
+            }
+        }
+    });
 });
 
 // Create a user
 app.post('/user', function (req, res) {
-	res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Origin', '*');
 
-	var username = req.body.username;
+    var username = req.body.username;
 
-	// check that the users did not
-	users.find({
-		username: username
-	}, function (err, docs) {
-		if (docs.length != 0) {
-			res.send(403);
-			return;
-		}
+    // check that the users did not
+    users.find({
+        username: username
+    }, function (err, docs) {
+        if (docs.length != 0) {
+            res.send(403);
+            return;
+        }
 
-		users.insert({
-			username: username,
-			password: sha1(req.body.password)
-		});
+        users.insert({
+            username: username,
+            password: sha1(req.body.password)
+        });
 
-		// output the user back to the client
-		users.find({
-			username: username
-		}, function (err, docs) {
-			var user = docs[0];
-			var key = session.createSession(user._id);
+        // output the user back to the client
+        users.find({
+            username: username
+        }, function (err, docs) {
+            var user = docs[0];
+            var key = session.createSession(user._id);
 
-			console.log('User ' + user.username + ' authenticated. Session id: ' + key);
+            console.log('User ' + user.username + ' authenticated. Session id: ' + key);
 
-			res.json({
-				key: key
-			});
-		});
-	});
+            res.json({
+                key: key
+            });
+        });
+    });
 });
 
 // Checks if a user exists
 app.post('/checkuser', function (req, res) {
-	res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Origin', '*');
 
-	var username = req.body.username;
+    var username = req.body.username;
 
-	// check that the users did not
-	users.find({
-		username: username
-	}, function (err, docs) {
-		if (docs.length != 0) {
-			res.send(403);
-			return;
-		} else {
-			res.json({});
-		}
-	});
+    // check that the users did not
+    users.find({
+        username: username
+    }, function (err, docs) {
+        if (docs.length != 0) {
+            res.send(403);
+            return;
+        } else {
+            res.json({});
+        }
+    });
 });
 
 app.post('/position', function (req, res) {
-	res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Origin', '*');
 
-	session.findSession(req.body.key, function(userid) {
-		if (userid == null){
-			res.send(403);
-			return;
-		}
+    session.findSession(req.body.key, function (userid) {
+        if (userid == null) {
+            res.send(403);
+            return;
+        }
 
-		var requestLat = req.body.lat;
-		var requestLon = req.body.lon;
+        var requestLat = req.body.lat;
+        var requestLon = req.body.lon;
 
-		// find their last position - if they had a last position then check that it was not too
-		// close to where they are currently
-		points.find({
-			userid: userid
-		}, {
-			limit: 1,
-			sort: {
-				time: -1
-			}
-		}, function (err, docs) {
+        // find their last position - if they had a last position then check that it was not too
+        // close to where they are currently
+        points.find({
+            userid: userid
+        }, {
+            limit: 1,
+            sort: {
+                time: -1
+            }
+        }, function (err, docs) {
 
-			if (docs.length != 0) {
-				var point = docs[0];
+            if (docs.length != 0) {
+                var point = docs[0];
 
-				var dist = Math.abs(math.geodist(parseFloat(requestLat), parseFloat(requestLon), parseFloat(point.lat), parseFloat(point.lon)));
-				console.log("Point received; distance to last point = " + dist + "m");
+                var dist = Math.abs(math.geodist(parseFloat(requestLat), parseFloat(requestLon), parseFloat(point.lat), parseFloat(point.lon)));
+                console.log("Point received; distance to last point = " + dist + "m");
 
-				if (dist > 10 && dist < 20) {
-					requestLat = point.lat;
-					requestLon = point.lon;
-				}
-			}
+                if (dist > 10 && dist < 20) {
+                    requestLat = point.lat;
+                    requestLon = point.lon;
+                }
+            }
 
-			points.insert({
-				userid: userid,
-				lat: parseFloat(requestLat),
-				lon: parseFloat(requestLon),
-				time: parseInt(new Date().getTime() / 1000)
-			});
+            points.insert({
+                userid: userid,
+                lat: parseFloat(requestLat),
+                lon: parseFloat(requestLon),
+                time: parseInt(new Date().getTime() / 1000)
+            });
 
-		});
-	});
+        });
+    });
 });
 
 app.post('/places', function (req, res) {
     res.setHeader('Access-Control-Allow-Origin', '*');
 
-    session.findSession(req.body.key, function(userid) {
-        if (userid == null){
+    session.findSession(req.body.key, function (userid) {
+        if (userid == null) {
             res.send(403);
             return;
         }
@@ -170,44 +170,47 @@ app.post('/places', function (req, res) {
         var requestLat = req.body.lat;
         var requestLon = req.body.lon;
 
+        points.find({
+            userid: userid,
+            "places.name": requestPlaceName
+        }, function (err, docs) {
 
-        // find their last position - if they had a last position then check that it was not too
-        // close to where they are currently
+            if (docs.length == 0) {
 
-
-        places.update(
-            { user: userid },
-            { $push: { places: { name: requestPlaceName,
+                places.update(
+                    { user: userid },
+                    { $push: { places: { name: requestPlaceName,
                         lat: parseFloat(requestLat),
                         lon: parseFloat(requestLon)
-            }}},{ upsert: true}
-        );
+                    }}}, { upsert: true}
+                );
+            }
 
-
+        });
     });
 });
 
 app.get('/points/:key', function (req, res) {
-	res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Origin', '*');
 
-	session.findSession(req.params.key, function (userid) {
-		if (userid == null) {
-			res.send(403);
-			return;
-		}
+    session.findSession(req.params.key, function (userid) {
+        if (userid == null) {
+            res.send(403);
+            return;
+        }
 
-		// just view points for the last 24 hours for now
-		var minTime = parseInt(new Date().getTime() / 1000) - 86400;
+        // just view points for the last 24 hours for now
+        var minTime = parseInt(new Date().getTime() / 1000) - 86400;
 
-		points.find({
-			userid: userid,
-			time: {
-				$gte: minTime
-			}
-		}, 'lat lon time -_id', function (err, docs) {
-			res.json(docs);
-		});
-	});
+        points.find({
+            userid: userid,
+            time: {
+                $gte: minTime
+            }
+        }, 'lat lon time -_id', function (err, docs) {
+            res.json(docs);
+        });
+    });
 });
 
 app.listen(8001);
